d4425e92964d7451f7114f90f3d29bcb01ed3dea
lujain <lujainm.com>
2023-11-16 15:03:37 -0800

    Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to print arry by bubble sort
+ * @array: the array well sorted
+ * @size: the size of arry
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #define SORT_H
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  * struct
@@ -29,7 +29,7 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
+void bubble_sort(int *array, size_t size);
 
 
 #endif
