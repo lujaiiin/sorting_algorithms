54fff364998353d804cfac18c7f198d0db296568
lujain <lujainm.com>
2023-11-16 15:10:04 -0800

    Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp = 0;
 
+	if (!array || size <= 1)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
