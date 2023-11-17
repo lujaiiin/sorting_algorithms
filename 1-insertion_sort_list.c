fecee376edda65007c5df3cfc8b4ba50749957b0
ahmed <ahelmyeng1@gmail.com>
2023-11-17 05:22:03 -0800

    task

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,22 +9,19 @@
 
 listint_t *switch_node(listint_t *node1, listint_t *node2)
 {
-listint_t *tmp = NULL;
+listint_t *prev_node = node1->prev;
+listint_t *next_node = node2->next;
 
-if (node2->next)  /*handle last node*/
-{
-tmp = node2->next;
-node2->next->prev = node1;
-}
-if (node1->prev) /*handle first node*/
-node1->prev->next = node2;
-else
-node2->prev = NULL;
+if (prev_node)
+prev_node->next = node2;
+if (next_node)
+next_node->prev = node1;
 
-node2->prev = node1->prev;
-node2->next = node1;
+node1->next = next_node;
 node1->prev = node2;
-node1->next = tmp;
+node2->next = node1;
+node2->prev = prev_node;
+
 return (node2);
 }
 
