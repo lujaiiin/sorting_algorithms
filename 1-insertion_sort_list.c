81994d3a0fbe51adca4351c3dbb003b72e1d722a
ahmed <ahelmyeng1@gmail.com>
2023-11-17 09:17:27 -0800

    task

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,19 +9,18 @@
 
 listint_t *switch_node(listint_t *node1, listint_t *node2)
 {
-listint_t *prev_node = node1->prev;
-listint_t *next_node = node2->next;
 
-if (prev_node)
-prev_node->next = node2;
-if (next_node)
-next_node->prev = node1;
+if (node2->next) /* handle node next to node2*/
+node2->next->prev = node1;
 
-node1->next = next_node;
+if (node1->prev) /* handle node previou to node1*/
+node1->prev->next = node2;
+
+node1->next = node2->next;
+node2->prev = node1->prev;
+
 node1->prev = node2;
 node2->next = node1;
-node2->prev = prev_node;
-
 return (node2);
 }
 
@@ -35,25 +34,26 @@ void insertion_sort_list(listint_t **list)
 {
 
 listint_t *h, *key, *prv_node;
-h = *list;
-if (!h->next || !h)
+
+if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
+h = *list;
+
 h = h->next;
-while (h) /*loop from second node*/
+while (h) /*loop starts from second node*/
 {
-key = h; /*second node*/
+key = h;
 h = h->next; /*enusre fixing h of swap*/
+
+if (!key)
+return;
 
 while (key->prev) /*switch nodes*/
 {
 prv_node = key->prev;
 if (key->n < prv_node->n)
 {
-
-if (!prv_node->prev)
-*list = key;
-
 key = switch_node(prv_node, key);
 if (!key->prev)
 *list = key;
