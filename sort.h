4439edee04340064dd5fb8d5bebcdd8a50badbdd
ahmed <ahelmyeng1@gmail.com>
2023-11-17 00:51:59 -0800

    tasks

--- sort.h
+++ sort.h
@@ -14,18 +14,26 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
+/*helper funcion*/
+void swp(int *x, int *y);
 
 
-
-
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*tasks functions*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+
 
 
 #endif
