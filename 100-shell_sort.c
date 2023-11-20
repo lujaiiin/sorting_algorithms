5e703b7733ff6609b6390710cb1054db63a81315
lujain <lujainm.com>
2023-11-20 11:25:14 -0800

    Sorting algorithms & Big O

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -15,7 +15,7 @@ void shell_sort(int *array, size_t size)
 	for (gap = size/2; gap > 0; gap /= 2)
 	{
 		for (i = gap; i < size; i += 1)
-		`{
+		{
 			temp = array[i];
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 				array[j] = array[j - gap];
